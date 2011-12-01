28808b3b9a57223dc888292e2a37087c75f2ed78
Jigyasa Makkar <Jigyasa_Makkar@external.mckinsey.com>
2011-12-01 15:50:02 +0530

    Hephzibah/Garima - Adding errors in data_inspector

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'http://rubygems.org'
 
 # Specify your gem's dependencies in data_sanity.gemspec
 gemspec
+
+group :test do
+  gem "database_cleaner"
+end
--- spec/data_sanity/inspector_spec.rb
+++ spec/data_sanity/inspector_spec.rb
@@ -22,6 +22,23 @@ describe "DataSanity::Inspector" do
       DataInspector.count.should == 0
     end
 
+    it "should check for errors on the model and populate fields in DataInspector" do
+      inspector = DataSanity::Inspector.new
+
+      Person.new(:name => "Valid-Record", :age => 20).save(:validate => false)
+      Person.new(:age => 1).save(:validate => false)
+
+      inspector.investigate
+      DataInspector.count.should == 1
+
+      first_person = Person.find_by_age(1)
+      first_person.valid?
+      DataInspector.first.table_name.should == "Person"
+      DataInspector.first.table_primary_key.should == "id"
+      DataInspector.first.primary_key_value.should == first_person.id.to_s
+      YAML.load(DataInspector.first.validation_errors).should == first_person.errors.full_messages
+    end
+
     after :each do
       clean_data_inspector_migration
     end
