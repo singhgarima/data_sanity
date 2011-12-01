7626344e56befb64b84fb9f057756fff4c2d7e98
Garima Singh <garima.slideshare@gmail.com>
2011-12-01 17:30:35 +0530

    Garima/Hephzibah - Adding datainspector model to gem only

--- lib/data_sanity/tasks.rb
+++ lib/data_sanity/tasks.rb
@@ -3,14 +3,14 @@ namespace :db do
     desc 'Create data inspector model for data sanity results'
     task :migrate => :environment do
       Dir.chdir("#{Rails.root}") do
-        system "rails g model DataInspector table_name:string table_primary_key:string primary_key_value:string validation_errors:text"
+        system "rails g migration DataInspector table_name:string table_primary_key:string primary_key_value:string validation_errors:text"
       end
     end
 
     desc 'Destroy data inspector model for data sanity results'
     task :rollback do
       Dir.chdir("#{Rails.root}") do
-        system "rails destroy model DataInspector"
+        system "rails destroy migration DataInspector"
       end
     end
   end
