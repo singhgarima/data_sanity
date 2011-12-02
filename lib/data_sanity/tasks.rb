2d687b4ca36c74aa3ca058853364397899bad401
Jigyasa Makkar <Jigyasa_Makkar@external.mckinsey.com>
2011-12-02 12:29:52 +0530

    Jyotsna/Garima - Adding random pick-up for validations

--- lib/data_sanity/tasks.rb
+++ lib/data_sanity/tasks.rb
@@ -3,14 +3,16 @@ namespace :db do
     desc 'Create data inspector model for data sanity results'
     task :migrate => :environment do
       Dir.chdir("#{Rails.root}") do
-        system "rails g migration DataInspector table_name:string table_primary_key:string primary_key_value:string validation_errors:text"
+        system "rails g migration create_data_inspector"
+        system "rails g migration add_columns_to_data_inspector table_name:string table_primary_key:string primary_key_value:string validation_errors:text"
       end
     end
 
     desc 'Destroy data inspector model for data sanity results'
     task :rollback do
       Dir.chdir("#{Rails.root}") do
-        system "rails destroy migration DataInspector"
+        system "rails destroy migration add_columns_to_data_inspector"
+        system "rails destroy migration create_data_inspector"
       end
     end
   end
