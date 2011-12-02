028d9a802d16aff06d3918fcd6aab6aeeacd6e3f
Garima Singh <garima.slideshare@gmail.com>
2011-12-02 17:07:22 +0530

    Jyotsna/Garima - added criteria based validations configurable through yml file

--- lib/data_sanity/inspector.rb
+++ lib/data_sanity/inspector.rb
@@ -2,20 +2,25 @@ module DataSanity
   class Inspector
 
     CONSIDERED_RECORDS = 1
-    ALLOWED_VALIDATION = [:all, :random]
-    attr_accessor :all, :random, :models, :records_per_model
+    ALLOWED_VALIDATION = [:all, :random, :criteria]
+    attr_accessor :all, :random, :criteria, :models, :records_per_model
 
     def initialize options = {}
       options[:validate] == :random ? @random = true : @all = true
       @records_per_model = options[:records_per_model] || CONSIDERED_RECORDS
       @models = load_models
+      file_path = "#{Rails.root}/config/data_sanity_criteria.yml"
+      @criteria = File.exists?(file_path) ? (YAML.load File.open(file_path).read) : false
     end
 
     def investigate
       @models.each do |model_string|
         model = model_string.constantize
         validate_all(model) if @all
-        validate_random(model) if @random
+        if @random
+          validate_criteria(model, @criteria[model_string]) and return if @criteria && @criteria[model_string].present?
+          validate_random(model)
+        end
       end
     end
 
@@ -30,8 +35,18 @@ module DataSanity
     def validate_random(model)
       no_of_records = model.count
       @records_per_model.times do
-        instance = model.first(:limit => 1, :offset => rand(no_of_records))
+        instance = model.offset(rand(no_of_records)).first
         populate_if_invalid_record(instance, model)
+      end
+    end
+
+    def validate_criteria(model, criteria)
+      criteria.each do |attribute, values|
+        values.each do |value|
+          results = model.where(attribute.to_sym => value)
+          instance = results.offset(rand(results.count)).first
+          populate_if_invalid_record(instance, model)
+        end
       end
     end
 
