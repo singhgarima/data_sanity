cb6b363467218715c11654e293b9e1efdab21681
Garima Singh <garima.slideshare@gmail.com>
2011-12-02 12:29:52 +0530

    Jyotsna/Garima - Adding random pick-up for validations

--- lib/data_sanity/inspector.rb
+++ lib/data_sanity/inspector.rb
@@ -1,30 +1,47 @@
 module DataSanity
   class Inspector
 
+    CONSIDERED_RECORDS = 1
     ALLOWED_VALIDATION = [:all, :random]
-
-    attr_accessor :all, :random, :models
+    attr_accessor :all, :random, :models, :records_per_model
 
     def initialize options = {}
       options[:validate] == :random ? @random = true : @all = true
+      @records_per_model = options[:records_per_model] || CONSIDERED_RECORDS
       @models = load_models
     end
 
     def investigate
       @models.each do |model_string|
         model = model_string.constantize
-        model.all.each do |instance|
-          unless instance.valid?
-            DataInspector.create(:table_name => model_string,
-                                 :table_primary_key => model.primary_key,
-                                 :primary_key_value => instance.send(model.primary_key),
-                                 :validation_errors => instance.errors.full_messages.to_yaml)
-          end
-        end
+        validate_all(model) if @all
+        validate_random(model) if @random
       end
     end
 
     private
+
+    def validate_all(model)
+      model.all.each do |instance|
+        populate_if_invalid_record(instance, model)
+      end
+    end
+
+    def validate_random(model)
+      no_of_records = model.count
+      @records_per_model.times do
+        instance = model.first(:limit => 1, :offset => rand(no_of_records))
+        populate_if_invalid_record(instance, model)
+      end
+    end
+
+    def populate_if_invalid_record(instance, model)
+      DataInspector.create(:table_name => model.to_s,
+                           :table_primary_key => model.primary_key,
+                           :primary_key_value => instance.send(model.primary_key),
+                           :validation_errors => instance.errors.full_messages.to_yaml) unless instance.valid?
+
+    end
 
     def load_models
       Dir["#{Rails.root}/app/models/**/*.rb"].each { |file_path| require file_path rescue nil }
@@ -32,5 +49,6 @@ module DataSanity
       all_models.delete(DataInspector)
       all_models.collect(&:name)
     end
+
   end
 end
