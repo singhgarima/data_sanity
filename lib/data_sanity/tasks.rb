c334125cac62fc37e526ae4990bd86da753b534a
Garima Singh <garima.slideshare@gmail.com>
2011-12-05 14:22:34 +0530

    Garima - Fixing migration issues

--- lib/data_sanity/tasks.rb
+++ lib/data_sanity/tasks.rb
@@ -3,16 +3,14 @@ namespace :data_sanity do
     desc 'Create data inspector model for data sanity results'
     task :migrate => :environment do
       Dir.chdir("#{Rails.root}") do
-        system "rails g migration create_data_inspector"
-        system "rails g migration add_columns_to_data_inspector table_name:string table_primary_key:string primary_key_value:string validation_errors:text"
+        system "rails generate model DataInspector table_name:string table_primary_key:string primary_key_value:string validation_errors:text"
       end
     end
 
     desc 'Destroy data inspector model for data sanity results'
-    task :rollback do
+    task :rollback => :environment do
       Dir.chdir("#{Rails.root}") do
-        system "rails destroy migration add_columns_to_data_inspector"
-        system "rails destroy migration create_data_inspector"
+        system "rails destroy model DataInspector"
       end
     end
   end
@@ -22,5 +20,10 @@ namespace :data_sanity do
     Dir.chdir("#{Rails.root}") do
       system "cp #{SOURCE_PATH}/data_sanity/templates/data_sanity_criteria.yml #{Rails.root}/config/."
     end
+  end
+
+  desc 'Data Sanity run investigation'
+  task :investigate, :validate, :records_per_model do |t, args|
+    DataSanity::Inspector.new(args).investigate
   end
 end
