f69223e92fdf7683747ca514f5718549a13e91f1
Garima Singh <garima.slideshare@gmail.com>
2012-04-13 10:58:05 +0530

    Garima - Adding code to pick only models in criteris file when criteria file present

--- spec/data_sanity/inspector_spec.rb
+++ spec/data_sanity/inspector_spec.rb
@@ -54,19 +54,19 @@ describe "DataSanity::Inspector" do
         DataInspector.first.primary_key_value.should == first_person.id.to_s
         YAML.load(DataInspector.first.validation_errors).should == first_person.errors.full_messages
       end
-      
+
       it "should check for errors on the model and populate fields in DataInspector even if valid? raises exception" do
         inspector = DataSanity::Inspector.new
-        
+
         exception = Exception.new
-        
+
         booming_mock = mock(:person)
         booming_mock.should_receive(:valid?).and_raise(exception)
         Person.should_receive(:all).and_return([booming_mock])
 
         inspector.investigate
         DataInspector.count.should == 1
-        
+
         YAML.load(DataInspector.first.validation_errors).should_not be_blank
       end
     end
@@ -121,8 +121,8 @@ describe "DataSanity::Inspector" do
           Car.new(:name => "Santro", :make => "HHH", :color => "NotAColor", :person => Person.last).save(:validate => false)
 
           inspector.investigate
-          DataInspector.count.should == 3
-          DataInspector.all.collect(&:table_name).should == ["Car", "Person", "Person"]
+          DataInspector.count.should == 2
+          DataInspector.all.collect(&:table_name).should == ["Person", "Person"]
         end
 
         it "should check all distinct values of field for which a criteria exists" do
@@ -144,14 +144,19 @@ describe "DataSanity::Inspector" do
           update_data_sanity_criteria(criteria_car_make)
           inspector = DataSanity::Inspector.new(:validate => "random", :records_per_model => "2")
 
-          5.times { |i| Car.new(:name => "Car Name#{i}", :make => "Brand1").save(:validate => false) }
-          5.times { |i| Car.new(:name => "Car Name#{i}", :make => "Brand2").save(:validate => false) }
+          Car.new(:name => "Car Name1", :make => "Brand1").save(:validate => false)
+          Car.new(:name => "Car Name2", :make => "Brand2").save(:validate => false)
+          Car.new(:name => "Car Name3", :make => "Brand1").save(:validate => false)
+          Car.new(:name => "Car Name4", :make => "Brand2").save(:validate => false)
+          Car.new(:name => "Car Name5", :make => "Brand1").save(:validate => false)
+          Car.new(:name => "Car Name6", :make => "Brand2").save(:validate => false)
+          Car.new(:name => "Car Name7", :make => "Brand3").save(:validate => false)
 
           inspector.investigate
 
-          DataInspector.count.should == 4
-          DataInspector.all.collect(&:table_name).should == ["Car", "Car", "Car", "Car"]
-          Car.find(DataInspector.all.collect(&:primary_key_value)).collect(&:make).should == ["Brand1", "Brand1", "Brand2", "Brand2"]
+          DataInspector.count.should == 5
+          DataInspector.all.collect(&:table_name).uniq.should == ["Car"]
+          Car.find(DataInspector.all.collect(&:primary_key_value)).collect(&:make).sort.should == ["Brand1", "Brand1", "Brand2", "Brand2", "Brand3"]
         end
 
         it "should select minimum of records_per_model and records matching a criteria" do
@@ -164,6 +169,19 @@ describe "DataSanity::Inspector" do
           inspector.investigate
 
           DataInspector.count.should == 4
+        end
+
+        it "should be able to look at multiple criteria" do
+          update_data_sanity_criteria(criteria_multiple)
+          inspector = DataSanity::Inspector.new(:validate => "random", :records_per_model => "1")
+
+          2.times { |i| Car.new(:name => "Car Name#{i}", :make => "Brand1").save(:validate => false) }
+          2.times { Person.new(:name => "Saju").save(:validate => false) }
+
+          inspector.investigate
+
+          DataInspector.count.should == 2
+          DataInspector.all.collect(&:table_name).sort.should == ["Person", "Car"].sort
         end
 
         after :each do
@@ -180,6 +198,11 @@ describe "DataSanity::Inspector" do
   def criteria_car_make
     "Car:
   make"
+  end
+
+  def criteria_multiple
+    "Car:
+Person:"
   end
 
 end
